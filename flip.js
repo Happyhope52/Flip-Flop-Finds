547abf38a99f6d03e689c279ad1e547272677b9b
Happyhope52 <97978995+Happyhope52@users.noreply.github.com>
2022-03-02 12:24:29 +0000

    unfinished changes

---
+++ flip.js
@@ -0,0 +1,38 @@
+//<script> need to move this to html
+/*function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}*/
+// need to move this to html and link js page </script>
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += "responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+window.weatherWidgetConfig =  window.weatherWidgetConfig || [];
+window.weatherWidgetConfig.push({
+    selector:".weatherWidget",
+    apiKey:"N9FBVCN7X2962KTST85GHCSZ5", //Sign up for your personal key
+    location:"Frinton On Sea", //Enter an address
+    unitGroup:"metric", //"us" or "metric"
+    forecastDays:10, //how many days forecast to show
+    title:"Frinton On Sea", //optional title to show in the 
+    showTitle:true, 
+    showConditions:true
+});
+
+(function() {
+var d = document, s = d.createElement('script');
+s.src = 'https://www.visualcrossing.com/widgets/forecast-simple/weather-forecast-widget-simple.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
